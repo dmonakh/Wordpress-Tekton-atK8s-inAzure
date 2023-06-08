5316f8a39c17444a6641e5f4e4e33d83a24c0a7d
dmonakh <d.monakh@it-sprout.org>
2023-06-08 13:27:14 +0300

    fix var

--- terraform-azure/variables.tf
+++ terraform-azure/variables.tf
@@ -1,20 +1,25 @@
 variable "resource_group_name" {
 type = string
 description = "RG name in Azure"
+default = "aks_tf_test"
 }
 variable "location" {
 type = string
 description = "Resources location in Azure"
+default = "EastUS2"
 }
 variable "cluster_name" {
 type = string
 description = "AKS name in Azure"
+default = "AKSMondy2022"
 }
 variable "kubernetes_version" {
 type = string
 description = "Kubernetes version"
+default = "1.25.8"
 }
 variable "system_node_count" {
 type = number
 description = "Number of AKS worker nodes"
+default = 1
 }
